47f21be6643d4c9c5bd7386357b063b5858d8e03
Laiba60 <laibasaeed026@gmail.com>
2025-12-22 00:19:24 -0800

    Add the BAnner Deisgn

--- src/components/AboutBanner.jsx
+++ src/components/AboutBanner.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Bannerimage from "../assets/images/Bannerimage.avif";
 import AllBanner from "../assets/images/AllBanner.jpg";
+
 const AboutBanner = () => {
   return (
     <section
-      className="relative h-[45vh] sm:h-[55vh] lg:h-[65vh] bg-cover bg-center"
+      className="relative min-h-[60vh] sm:min-h-[70vh] lg:min-h-[85vh] 
+      bg-cover bg-center flex items-center"
       style={{ backgroundImage: `url(${AllBanner})` }}
     >
-      <div className="absolute inset-0 "></div>
-      <div className="relative z-10 max-w-7xl mx-auto h-full flex items-center px-4 sm:px-6 lg:px-8">
+      
+      <div className="absolute inset-0"></div>
+      <div className="relative z-10 max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8">
         <div className="text-left text-white">
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4">
             About Us
@@ -16,7 +18,7 @@ const AboutBanner = () => {
           <p className="text-sm sm:text-base text-gray-200">
             <span className="opacity-80">Home</span>
             <span className="mx-2">/</span>
-            <span className="font-semibold text-white">About Us</span>
+            <span className="font-semibold text-[#A58120]">About Us</span>
           </p>
         </div>
       </div>
